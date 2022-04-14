242e34c6449ec7de1cfd04830f69f8bb7ae79d58
Jakub Kleban <jakub.kleban@enovation.ie>
2022-04-14 17:03:21 +0200

    Complete translate widget

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
   {
@@ -13,6 +14,14 @@ const options = [
   {
     label: 'Hindi',
     value: 'hi'
+  },
+  {
+    label: 'Dutch',
+    value: 'nl'
+  },
+  {
+    label: 'Polish',
+    value: 'pl'
   }
 ];
 
@@ -33,6 +42,12 @@ const Translate = () => {
         selected={language}
         onSelectedChange={setLanguage}
         label="Select a Language"
+      />
+      <hr/>
+      <h3 className="ui header">Output</h3>
+      <Convert
+        language={language}
+        text={text}
       />
     </div>
   );
