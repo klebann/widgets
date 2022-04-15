adf2e5409beadf56207cb868d4db4c48d30b42df
Jakub Kleban <jakub.kleban@enovation.ie>
2022-04-15 10:36:54 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import Accordion from "./components/Accordion";
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 import items from "./data/items";
 import colors from "./data/colors";
 
 const App = () => {
+  const [selected, setSelected] = useState(colors[0]);
+
   return (
     <div>
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Selecty a color"
+          options={colors}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
